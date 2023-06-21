ba5b994acba0513e16c030b48f8a293deeaaae2e
ElectroCERP <carloscer27r@gmail.com>
2023-06-20 21:10:19 -0300

    preparando front para deploy 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import {store} from './redux/store.js'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://countries-4-u-back-production.up.railway.app/'
+axios.defaults.baseURL = 'https://countries-4-u-back-production.up.railway.app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
